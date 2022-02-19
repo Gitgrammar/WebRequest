c25ad460308f4bb3d75161f229d66fd12a6e1d7f
hogehoge <susumitosusyo@hotmail.com>
2022-02-19 12:47:15 +0900

    Http通信の終了

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -6,9 +6,9 @@ using UnityEngine.Networking;
 
 public class ButtonController : MonoBehaviour
 {
-    public InputField et1;
+    public InputField et1; //inputfield型. UIを置くとできる。
     public InputField et2;
-    public Text result;
+    public Text result;//textをコンポーネントを置く
 
     public void btClick()
     {
@@ -16,12 +16,12 @@ public class ButtonController : MonoBehaviour
         string y = et2.text;
         StartCoroutine(HttpConnect(x, y));
     }
-    IEnumerator HttpConnect(string x, string y)
+    IEnumerator HttpConnect(string x, string y)//文字列２つで受け取る
     {
         WWWForm form = new WWWForm();
-        form.AddField("x", x);
+        form.AddField("x", x); 
         form.AddField("y", y);
-        string url = "https://joytas.net/php/calc.php";
+        string url = "https://joytas.net/php/calc.php";//post通信でjoytasへ送る。
         UnityWebRequest uwr = UnityWebRequest.Post(url, form);
         yield return uwr.SendWebRequest();
         if (uwr.isHttpError || uwr.isNetworkError)
@@ -30,7 +30,7 @@ public class ButtonController : MonoBehaviour
         }
         else
         {
-            result.text = uwr.downloadHandler.text;
+            result.text = uwr.downloadHandler.text; //通信したときの結果をresult.textで代入.
             Debug.Log(result.text);
         }
     }
