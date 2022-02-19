9d533efae3d8a3a1628f73d85878377c37ae8b19
hogehoge <susumitosusyo@hotmail.com>
2022-02-19 11:51:25 +0900

    計算終わる

--- Assets/Scripts/ImageController.cs
+++ Assets/Scripts/ImageController.cs
@@ -24,11 +24,13 @@ public class ImageController : MonoBehaviour
         else
         {
 
-            Texture texture = DownloadHandlerTexture.GetContent(uwr);
+            Texture texture = DownloadHandlerTexture.GetContent(uwr);//textureは単なる画像
+            //単なる画像をスプライトに変える。キャンバスは中心の概念があるため変える必要がある。
 
-            Sprite sp = Sprite.Create((Texture2D)texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-
-            Image image = GetComponent<Image>();
+            Sprite sp = Sprite.Create((Texture2D)texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));//スプライトに変換する。
+                                                                                                                                  //　１引数がダウンロードしたtextureダウンキャスト。ダウンキャストはもう決まり。もともとの画像第二引数。第三引数はピボット
+            sp.name = "man";
+            Image image = GetComponent<Image>();//imagecomponent 取得
 
             image.rectTransform.sizeDelta = new Vector2(texture.width, texture.height);
 
--- Assets/Scripts/PostController.cs
+++ Assets/Scripts/PostController.cs
@@ -6,7 +6,7 @@ using UnityEngine.Networking;
 
 public class PostController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         StartCoroutine(HttpConnect());
@@ -14,21 +14,21 @@ public class PostController : MonoBehaviour
     }
     IEnumerator HttpConnect()
     {
-        WWWForm form = new WWWForm();
-        form.AddField("x", 5);
-        form.AddField("y", 8);
+        WWWForm form = new WWWForm();//昔の送信したいときの名残を書く。インスタンスを作る。post通信はWWWForm使う。
+        form.AddField("x", 5);//フィールドを追加していく。x=5というフィールドに追加それでサーバー側に置く
+        form.AddField("y", 8);//hashmap と一緒.第一引数はキー　第二は値。第二は小数点入れるときは文字列にする。requestparameter は文字列になる。
 
         string url= "https://joytas.net/php/calc.php";
-        UnityWebRequest uwr = UnityWebRequest.Post(url,form);
-        yield return uwr.SendWebRequest();
+        UnityWebRequest uwr = UnityWebRequest.Post(url,form);//リクエストとリスポンスを構成するモジュラーを提供する
+        yield return uwr.SendWebRequest();//sendwebrequest 送受信開始
         if (uwr.isHttpError || uwr.isNetworkError)
         {
-            Debug.Log(uwr.error);
+            Debug.Log(uwr.error);//コンソールに文字を表す
 
         }
         else
         {
-            Debug.Log(uwr.downloadHandler.text);
+            Debug.Log(uwr.downloadHandler.text);//ダウンロードハンドラーでデータの送信処理を行う。
         }
     }
 
